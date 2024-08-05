1686692a89f339ba23591786449c6c51d18f999b
Nurtas <nurtas.aristan@gmail.com>
2024-08-06 00:02:48 +0500

    Model Article - fillable, users()

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,6 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class Article extends Model {
-    
+
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'content',
+    ];
+
+    public function users() {
+        return $this->belongsToMany(User::class);
+    }
+
 }
