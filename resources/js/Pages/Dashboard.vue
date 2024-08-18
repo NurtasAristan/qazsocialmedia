3b1d2f223390f84652b29c43468473828b9d465c
Nurtas <nurtas.aristan@gmail.com>
2024-08-18 16:31:28 +0500

    Update 1

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -9,8 +9,11 @@ defineProps({
 
 <template>
     <AppLayout title="Dashboard" :categories="categories">
-        <div v-for="article in myArticles" :key="article.id" class="m-4 p-4 w-64 rounded border-2">
-            <li v-for="category in article.categories">
+        <div v-for="article in myArticles" :key="article.id" class="m-4 p-2 w-64 rounded border-2">
+            <div class="border-b-2">
+                <span class="material-symbols-outlined rounded-full border-2">more_horiz</span>
+            </div>
+            <li class="inline-block px-2 border-2 rounded list-none" v-for="category in article.categories">
                 <p>{{ category.category }}</p>
             </li>
             <p class="font-bold">{{ article.title }}</p>
