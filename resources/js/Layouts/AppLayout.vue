e76b8ab4018c86fe31c477a854d3cb4b5893c85d
Nurtas <nurtas.aristan@gmail.com>
2024-08-24 00:26:57 +0500

    AppLayout: nav+post change1

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -278,7 +278,7 @@ const form = useForm({
     </nav>
 
     <!-- Sidebar Navigation -->
-    <aside class="fixed top-0 z-30 bg-white shadow w-64 h-screen pt-20 border-2">
+    <aside class="fixed top-0 z-30 bg-yellow-300 shadow w-64 h-screen pt-20 border-2">
         <ul class="px-3 font-medium">
             <li>
                 <a href="#" class="flex p-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
@@ -293,8 +293,8 @@ const form = useForm({
         </ul>
     </aside>
 
-    <div v-show="showPostForm" class="absolute z-50 flex justify-center items-center h-screen bg-yellow-300">
-        <form @submit.prevent="form.post('/dashboard')" class="w-full max-w-md rounded border-2">
+    <div v-show="showPostForm" class="w-full absolute z-50 flex justify-center items-center h-screen bg-transparent">
+        <form @submit.prevent="form.post('/dashboard')" class="w-full max-w-md rounded border-2 bg-yellow-300">
             <div class="p-4 mb-5">
                 <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
                 <select v-model="form.category_id" id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Title" required>
